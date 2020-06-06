510c43ae3a42f7c1a7cc94f1675c2f91a9b3d5ec
wener <wenermail@gmail.com>
2020-06-07 02:39:01 +0800

    fixing ci

--- docker-build.sh
+++ docker-build.sh
@@ -3,4 +3,4 @@
 QUIET=1 . scripts/preset.sh
 . scripts/docker-run.sh
 
-DOCKER_CMD="env verbose=1 profile=${profile} conf=${conf} FLAVOR=${FLAVOR} ./build.sh" docker-run
+DOCKER_MODE=i DOCKER_CMD="env verbose=1 profile=${profile} conf=${conf} FLAVOR=${FLAVOR} ./build.sh" docker-run
